d07aff70fbc22df2a4593b08a1d211231e2700ce
wendipratamas876-gif <wendipratamas876@gmail.com>
2025-12-24 23:01:41 +0700

    Implement AttackManager class for managing attacks

--- public/js/attack.js
+++ public/js/attack.js
@@ -1,1 +1,66 @@
+class AttackManager {
+    constructor() {
+        this.ongoingAttacks = [];
+        this.attackHistory = [];
+        this.attackMethods = [];
+        this.loadAttackMethods();
+    }
+    
+    loadAttackMethods() {
+        fetch('/api/config')
+            .then(response => response.json())
+            .then(data => {
+                this.attackMethods = data.attackMethods;
+            });
+    }
+    
+    launchAttack(target, method, duration) {
+        const attack = {
+            id: Date.now(),
+            target,
+            method,
+            duration,
+            startTime: new Date(),
+            status: 'running'
+        };
+        
+        this.ongoingAttacks.push(attack);
+        this.saveAttack();
+        
+        // Simulate attack completion
+        setTimeout(() => {
+            attack.status = 'completed';
+            attack.endTime = new Date();
+            this.attackHistory.push(attack);
+            this.ongoingAttacks = this.ongoingAttacks.filter(a => a.id !== attack.id);
+            this.saveAttack();
+        }, duration * 1000);
+    }
+    
+    stopAllAttacks() {
+        this.ongoingAttacks.forEach(attack => {
+            attack.status = 'stopped';
+            attack.endTime = new Date();
+        });
+        this.attackHistory.push(...this.ongoingAttacks);
+        this.ongoingAttacks = [];
+        this.saveAttack();
+    }
+    
+    saveAttack() {
+        // Save to local storage or send to server
+        localStorage.setItem('attacks', JSON.stringify({
+            ongoing: this.ongoingAttacks,
+            history: this.attackHistory
+        }));
+    }
+    
+    getAttackMethods() {
+        return this.attackMethods;
+    }
+}
 
+// Export for use in other modules
+if (typeof module !== 'undefined') {
+    module.exports = AttackManager;
+}
