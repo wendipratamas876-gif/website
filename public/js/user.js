6bd3ac945c74bf3421affd30d4508d540e781c91
wendipratamas876-gif <wendipratamas876@gmail.com>
2025-12-24 23:00:46 +0700

    Update user.js

--- public/js/user.js
+++ public/js/user.js
@@ -1,1 +1,39 @@
+class UserManagement {
+    constructor() {
+        this.users = [];
+        this.loadUsers();
+    }
+    
+    loadUsers() {
+        fetch('/api/users')
+            .then(response => response.json())
+            .then(data => {
+                this.users = data;
+            });
+    }
+    
+    addUser(user) {
+        this.users.push(user);
+        this.saveUsers();
+    }
+    
+    saveUsers() {
+        fetch('/api/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.users)
+        });
+    }
+    
+    deleteUser(userId) {
+        this.users = this.users.filter(user => user.id !== userId);
+        this.saveUsers();
+    }
+}
 
+// Export for use in other modules
+if (typeof module !== 'undefined') {
+    module.exports = UserManagement;
+}
